2fd99cd35de626d636c180b038a5f27d76473fd3
zhangwanli <248803095@qq.com>
2019-01-22 09:30:09 +0800

    zdxf

--- src/main/resources/static/js/getrsakey.js
+++ src/main/resources/static/js/getrsakey.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
 var publicKey="";getRSAkey();function getRSAkey(){$.ajax({type:"get",url:location.protocol+"//user.tuwan.com/api/method/getpkey",dataType:"jsonp",jsonp:"callback",success:function(a){publicKey=1037==a.code?a.data:""}})};
-=======
-﻿var publicKey="";getRSAkey();function getRSAkey(){$.ajax({type:"get",url:location.protocol+"//user.tuwan.com/api/method/getpkey",dataType:"jsonp",jsonp:"callback",success:function(a){publicKey=1037==a.code?a.data:""}})};
->>>>>>> 62f535f901c014b9188b44f6c4b3b6f12f87f396
+
